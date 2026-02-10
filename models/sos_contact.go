4f42a9985ffaa557221f31c00922c41ebb9f128d
MinhDao-boop <daominhchvk36@gmail.com>
2026-02-10 14:18:58 +0700

    Update: add index to boost db query speed

--- models/sos_contact.go
+++ models/sos_contact.go
@@ -5,6 +5,6 @@ type SOSContact struct {
 	Name     string `gorm:"type:varchar(50);not null" json:"name;required"`
 	Role     string `gorm:"type:varchar(50);not null" json:"role;required"`
 	Phone    string `gorm:"type:varchar(20);not null" json:"phone;required"`
-	IsActive bool   `gorm:"type:tinyint(1);not null" json:"is_active;required"`
+	IsActive bool   `gorm:"type:tinyint(1);not null;index" json:"is_active;required"`
 	Note     string `gorm:"type:text" json:"note"`
 }
